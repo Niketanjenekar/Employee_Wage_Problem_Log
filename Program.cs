c6ba1323c9dd4a2b927f2b4bd66dd85efe347ccb
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-18 12:41:23 +0530

    non static employee wage problem

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeWageComputationalProblem;
+
+namespace log_EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logging Employee Wage Problem");
+
+        }
+    }
+}
