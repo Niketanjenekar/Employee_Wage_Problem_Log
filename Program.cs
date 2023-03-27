955f289984e0c3ae04e45a7315653e90bc520ef0
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-27 15:10:13 +0530

    Saving for multiple companies for calculating wages use case 9

--- Program.cs
+++ Program.cs
@@ -32,10 +32,33 @@ namespace log_EmployeeWageProblem
             //obj.EmployeeWageUseCase7();
 
 
-            EmpWageforMultipleCompanies_UseCase8.EmployeeWageUC8("Dmart", 20, 10, 90);
-            EmpWageforMultipleCompanies_UseCase8.EmployeeWageUC8("Reliance", 40, 35, 80);
+            //EmpWageforMultipleCompanies_UseCase8.EmployeeWageUC8("Dmart", 20, 10, 90);
+            //EmpWageforMultipleCompanies_UseCase8.EmployeeWageUC8("Reliance", 40, 35, 80);
 
 
+            SavingMultipleCompany_Usecase9 savingmultiple = new SavingMultipleCompany_Usecase9();
+            Console.WriteLine("Enter the company Name : \n1. Company1\n2.Comany2\n3.Company3");
+            int company = Convert.ToInt32(Console.ReadLine());
+            switch (company)
+            {
+                case 1:
+                    savingmultiple.EmpWager1();
+                    savingmultiple.EmployeeWageUC9();
+                    break;
+                case 2:
+                    savingmultiple.EmpWager2();
+                    savingmultiple.EmployeeWageUC9();
+                    break;
+                case 3:
+                    savingmultiple.EmpWager3();
+                    savingmultiple.EmployeeWageUC9();
+                    break;
+                default:
+                    Console.WriteLine("The company you entered is not correct");
+                    break;
+
+            }
+            
         }
     }
 }
---
+++ SavingMultipleCompany_Usecase9.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log_EmployeeWageProblem
+{
+    public class SavingMultipleCompany_Usecase9
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+
+
+        public void EmpWager1()
+        {
+            empRatePerHour = 350;
+            numOfWorkingDays = 10;
+            maxHoursPerMonth = 200;
+            Console.WriteLine("Total Company Wage: " + (empRatePerHour * maxHoursPerMonth));
+        }
+
+        public void EmpWager2()
+        {
+            empRatePerHour = 500;
+            numOfWorkingDays = 23;
+            maxHoursPerMonth = 320;
+            Console.WriteLine("Total Company Wage: " + (empRatePerHour * maxHoursPerMonth));
+        }
+        public void EmpWager3()
+        {
+            empRatePerHour = 700;
+            numOfWorkingDays = 16;
+            maxHoursPerMonth = 376;
+            Console.WriteLine("Total Company Wage: " + (empRatePerHour * maxHoursPerMonth));
+        }
+        public void EmployeeWageUC9()
+        {
+            
+            int Emp_Hrs = 0;
+            int Emp_Wage = 0;
+
+            Random random = new Random();
+
+            int Emp_check = random.Next(3);
+
+            switch (Emp_check)
+            {
+                case IS_FULL_TIME:
+                    Emp_Hrs = 8;
+                    Console.WriteLine("The Employee is Present");
+                    break;
+                case IS_PART_TIME:
+                    Emp_Hrs = 4;
+                    Console.WriteLine("The Employee is Present for Part Time");
+                    break;
+                default:
+                    Emp_Hrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
+
+
+            }
+            Emp_Wage = Emp_Hrs * empRatePerHour * numOfWorkingDays;
+            Console.WriteLine("The Employee Wage for " + company + " is :" + Emp_Wage + " Rupees");
+            
+        }
+    }
+       
+}
+
+
